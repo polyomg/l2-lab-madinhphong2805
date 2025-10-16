2bd6ac3f07bf90d694c4979cf63c99f151651089
madinhphong2805 <madinhphong2805@gmail.com>
2025-10-16 10:11:11 +0700

    Add files via upload

---
+++ Lab1/src/src/main/java/poly/edu/Ps39422Lab5Application.java
@@ -0,0 +1,13 @@
+package poly.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps39422Lab5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps39422Lab5Application.class, args);
+	}
+
+}
---
+++ Lab1/src/src/test/java/poly/edu/Ps39422Lab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package poly.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ps39422Lab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
